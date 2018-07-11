c9430d24e4939df61aa90f38cee26bc039b94fb5
liangjiang <xuan03121994@163.com>
2018-07-11 16:27:08 +0800

    优化新闻修改前端展示

--- www/admin/js/controller/NewsModule/newsEditModule.js
+++ www/admin/js/controller/NewsModule/newsEditModule.js
@@ -32,60 +32,60 @@ angular.module('app')
                     msgInit();
                     // 富文本
                     CKEDITOR.instances. content .setData($scope.news.content);
-                }
+                };
+
+                // 富文本编辑器配置
+                CKEDITOR.replace('content',{
+                    toolbar:[
+                        ['Undo','Redo','-','Find','-','SelectAll','RemoveFormat'],
+                        ['FontSize'],['Bold', 'Italic'],['TextColor','BGColor'],
+                        ['NumberedList','BulletedList'],
+                        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+                        ['Link','Unlink'],
+                        ['Table','HorizontalRule','Smiley','-',/*'Image',*/'Maximize']
+                    ],
+                    height: 250,
+                    removePlugins: 'elementspath' // 移除编辑器底部状态栏显示的元素路径和调整编辑器大小的按钮 elementspath,resize
+                    // removeDialogTabs: '/uploads', // 配置图片上传路径
+                    // filebrowserImageUploadUrl: '/uploads?type=images'
+                });
+
+                // 发布时间控制
+                $scope.dataIn = function(){
+                    $scope.dates=null;
+                    if($scope.now){
+                        $scope.datetime=false;
+                    }
+                };
+                $scope.dataOut = function(){
+                    if($scope.timing){
+                        $scope.datetime=true;
+                    }
+                };
+
+                // 修改新闻
+                $scope.newsEdit=function(){
+                    $('#newsEditId').ajaxForm({
+                        beforeSend:function(){
+                            $scope.EDITNEWSIF = true;
+                        },
+                        success:function(data){
+                            G.expire(data);
+                            $scope.EDITNEWSIF = false;
+                            $('.alerts .modal-body').text(data.msg);
+                            $('.alerts').modal('show');
+                            if(data.i){
+                                $scope.Tip = function(){
+                                    // 跳转到列表
+                                    setTimeout(function(){
+                                        $state.go('news')
+                                    },500);
+                                }
+                            }
+                        }
+                    });
+                };
             })();
         });
-
-        // 富文本编辑器配置
-        CKEDITOR.replace('content',{
-            toolbar:[
-                ['Undo','Redo','-','Find','-','SelectAll','RemoveFormat'],
-                ['FontSize'],['Bold', 'Italic'],['TextColor','BGColor'],
-                ['NumberedList','BulletedList'],
-                ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-                ['Link','Unlink'],
-                ['Table','HorizontalRule','Smiley','-',/*'Image',*/'Maximize']
-            ],
-            height: 250,
-            removePlugins: 'elementspath' // 移除编辑器底部状态栏显示的元素路径和调整编辑器大小的按钮 elementspath,resize
-            // removeDialogTabs: '/uploads', // 配置图片上传路径
-            // filebrowserImageUploadUrl: '/uploads?type=images'
-        });
-
-        // 发布时间控制
-        $scope.dataIn = function(){
-            $scope.dates=null;
-            if($scope.now){
-                $scope.datetime=false;
-            }
-        };
-        $scope.dataOut = function(){
-            if($scope.timing){
-                $scope.datetime=true;
-            }
-        };
-
-        // 修改新闻
-        $scope.newsEdit=function(){
-            $('#newsEditId').ajaxForm({
-                beforeSend:function(){
-                    $scope.EDITNEWSIF = true;
-                },
-                success:function(data){
-                    G.expire(data);
-                    $scope.EDITNEWSIF = false;
-                    $('.alerts .modal-body').text(data.msg);
-                    $('.alerts').modal('show');
-                    if(data.i){
-                        $scope.Tip = function(){
-                            // 跳转到列表
-                            setTimeout(function(){
-                                $state.go('news')
-                            },500);
-                        }
-                    }
-                }
-            });
-        };
 
     }]);
