785747fee78593c5d59119282ede2d0e40a6d8ce
liangjiang <xuan03121994@163.com>
2018-07-13 15:12:50 +0800

    商品图片上传功能添加

--- router/admin/goods.js
+++ router/admin/goods.js
@@ -1,13 +1,28 @@
 /* 商品模块 */
 
 const router=require('koa-router')();
+const koaBody=require('koa-body');
+const paths=require('path');
 const moment=require('moment');
 const goodsModel=require('../../model/goods');
 const info=require('../../middlewares/info');
 
 router
     // 添加商品
-    .post('/goodsadd', async ()=>{
+    .post('/goodsadd', koaBody({
+        multipart: true,
+        formidable: {
+            uploadDir: paths.join(__dirname, '../../www/uploads/goods'),
+            keepExtensions: true,
+            maxFieldsSize: 20 * 1024 * 1024, // 20M
+            maxFileSize: 200 * 1024 * 1024 // 200M
+        },
+        onError(err){
+            console.log('文件上传失败！',err);
+        }
+    }), async (ctx)=>{
+        ctx.body='test';
+        console.log(ctx.request.files,ctx.request.body);
 
     })
 
--- www/admin/js/controller/GoodsManage/goodsAddModel.js
+++ www/admin/js/controller/GoodsManage/goodsAddModel.js
@@ -7,90 +7,6 @@ angular.module('app')
         $anchorScroll();
 
         // 产品图片上传
-        $(function() {
-            var delParent,
-                defaults = {
-                    fileType: ["jpg", "png", "bmp", "jpeg"],
-                    fileSize: 1024 * 1024 * 10
-                };
-            $(".file").change(function() {
-                var idFile = $(this).attr("id"),
-                    file = document.getElementById(idFile),
-                    imgContainer = $(this).parents(".z_photo"),
-                    fileList = file.files,
-                    input = $(this).parent(),
-                    imgArr = [],
-                    numUp = imgContainer.find(".up-section").length,
-                    totalNum = numUp + fileList.length;
-                if (fileList.length > 5 || totalNum > 5) {
-                    alert("上传图片数目不可以超过5个，请重新选择");
-                } else if (numUp < 5) {
-                    fileList = validateUp(fileList);
-                    for (var i = 0; i < fileList.length; i++) {
-                        var imgUrl = window.URL.createObjectURL(fileList[i]);
-                        imgArr.push(imgUrl);
-                        var $section = $("<section class='up-section fl loading'>");
-                        imgContainer.prepend($section);
-                        var $span = $("<span class='up-span'>");
-                        $span.appendTo($section);
-                        var $img0 = $("<img class='close-upimg'>").on("click", function(event) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                            // $(".works-mask").show();
-                            delParent = $(this).parent();
-                            var numUp = delParent.siblings().length;
-                            if (numUp < 6) {
-                                delParent.parent().find(".z_file").show();
-                            }
-                            delParent.remove();
-                        });
-                        $img0.attr("src", "img/a7.png").appendTo($section);
-                        var $img = $("<img class='up-img up-opcity'>");
-                        $img.attr("src", imgArr[i]);
-                        $img.appendTo($section);
-                        var $p = $("<p class='img-name-p'>");
-                        $p.html(fileList[i].name).appendTo($section);
-                        var $input = $("<input id='taglocation' name='taglocation' value='' type='hidden'>");
-                        $input.appendTo($section);
-                        var $input2 = $("<input id='tags' name='tags' value='' type='hidden'/>");
-                        $input2.appendTo($section);
-                    }
-                }
-                setTimeout(function() {
-                    $(".up-section").removeClass("loading");
-                    $(".up-img").removeClass("up-opcity");
-                }, 450);
-                numUp = imgContainer.find(".up-section").length;
-                if (numUp >= 5) {
-                    $(this).parent().hide();
-                }
-                $(this).val("");
-            });
-
-            function validateUp(files) {
-                var arrFiles = [];
-                for (var i = 0, file; file = files[i]; i++) {
-                    var newStr = file.name.split("").reverse().join("");
-                    if (newStr.split(".")[0] != null) {
-                        var type = newStr.split(".")[0].split("").reverse().join("");
-                        console.log(type + "===type===");
-                        if (jQuery.inArray(type, defaults.fileType) > -1) {
-                            if (file.size >= defaults.fileSize) {
-                                alert(file.size);
-                                alert('您这个"' + file.name + '"文件大小过大');
-                            } else {
-                                arrFiles.push(file);
-                            }
-                        } else {
-                            alert('您这个"' + file.name + '"上传类型不符合');
-                        }
-                    } else {
-                        alert('您这个"' + file.name + '"没有类型, 无法识别');
-                    }
-                }
-                return arrFiles;
-            }
-        });
 
         // 富文本编辑器配置
         CKEDITOR.replace('content',{
@@ -105,5 +21,29 @@ angular.module('app')
             height: 250,
             removePlugins: 'elementspath' // 移除编辑器底部状态栏显示的元素路径和调整编辑器大小的按钮 elementspath,resize
         });
+
+        // 添加商品
+        $scope.goodsAdd=function(){
+            $('#goodsAddId').ajaxForm({
+                beforeSend:function(){
+                    $scope.ADDGOODSIF = true;
+                },
+                success:function(data){
+                    G.expire(data);
+                    $scope.ADDGOODSIF = false;
+                    $('.alerts .modal-body').text(data.msg);
+                    $('.alerts').modal('show');
+                    if(data.i){
+                        $scope.Tip = function(){
+                            // 跳转到列表
+                            setTimeout(function(){
+                                $state.go('goods')
+                            },500);
+                        }
+                    }
+                }
+            });
+        };
+
 
     }]);
