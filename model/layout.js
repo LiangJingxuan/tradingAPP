f9a644c0d450fbd6c923c6d9eaac195a7bd2bcfa
liangjiang <xuan03121994@163.com>
2018-08-15 11:27:56 +0800

    layout common data build

--- model/layout.js
+++ model/layout.js
@@ -2,9 +2,13 @@ const sql=require('../config/default').sql;
 
 module.exports={
 
-    // 免费咨询信息添加
-    consultAdd(name,email,phone,content){
-        
-        return sql.query(`INSERT INTO customer (name,email,phone,content,reply) VALUES ('${name}','${email}','${phone}','${content}',0);`);
+    // 客户咨询信息添加
+    consultAdd(name='',email='',phone,content='',time){
+        return sql.query(`INSERT INTO customer (name,email,phone,content,reply,time) VALUES ('${name}','${email}','${phone}','${content}',0,${time});`);
+    },
+
+    // 公司信息查询
+    companyInfo(){
+        return sql.query(`SELECT * FROM company`);
     }
 };
--- router/layout/common.js
+++ router/layout/common.js
@@ -1,12 +1,34 @@
 /* 网站公共功能模块 */
 
 const router=require('koa-router')();
+const moment=require('moment');
 const layout=require('../../model/layout');
+const info=require('../../middlewares/info');
 
 router
-    // 免费咨询信息添加
+    // 客户咨询信息添加
     .post('/consult', async (ctx)=>{
+        const custom=ctx.request.body,
+              time=moment().unix();
+        try{
+            const data=await layout.consultAdd(custom.name,custom.email,custom.phone,custom.content,time);
+            if(data.affectedRows){
+                // 新增成功
+                ctx.body=info.suc('您的消息已发送成功，我们会在最短时间内给您答复！');
+            }else{
+                // 新增失败
+                ctx.body=info.err('您的消息发送失败，请重试！');
+            }
 
+        }catch (e) {
+            ctx.body=info.err('操作失败，请重试！');
+        }
+    })
+
+    // 获取公司信息
+    .get('/company', async (ctx)=>{
+        const data=await layout.companyInfo();
+        ctx.body=data[0];
     })
 
     ;
