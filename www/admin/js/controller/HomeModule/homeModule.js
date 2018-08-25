52ef65eaa7793f4c49e291eda68bf22c251396d6
liangjiang <xuan03121994@163.com>
2018-08-25 11:07:16 +0800

    admin module optimize

--- www/admin/js/controller/HomeModule/homeModule.js
+++ www/admin/js/controller/HomeModule/homeModule.js
@@ -6,4 +6,15 @@ angular.module('app')
         $location.hash('main-content');
         $anchorScroll();
 
+        // 用户数据列表
+        $http.get('/admin/user/userlist').success(function(data){
+            G.expire(data);
+            for(var i=0,len=data.length;i<len;i++){
+                if(data[i].id===parseInt(sessionStorage.getItem('uid'))){
+                    $scope.userItem=data[i];
+                }
+            }
+        });
+        $scope.copyright=sessionStorage.getItem('copyright');
+
     }]);
